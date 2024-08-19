9e7a9273042e1f0f0c044dc18b2f91765d0fa60c
AnaRFG <2304151@ucc.edu.ar>
2024-08-19 15:46:05 -0300

    archivos para resolver los ejercicios

--- Ej7.cpp
+++ Ej7.cpp
@@ -1,3 +1,17 @@
-//
-// Created by Usuario on 19/8/2024.
-//
+/*
+ * Utiliza la clase ListaDoble para gestionar una lista de estudiantes, donde cada estudiante tiene un nombre (cadena de caracteres)
+ * y una edad (entero). Implementa las siguientes funcionalidades:
+ * 1. Agregar Estudiantes: ○ UsalosmétodosdeListaDobleparaagregar varios estudiantes a la lista.
+ * Debes insertar al menos cinco estudiantes con nombres y edades diferentes.
+ * 2. Mostrar Estudiantes: ○ Implementa una función que recorra la lista y muestre todos los estudiantes en orden.
+ * Utiliza el método imprimir() de ListaDoble.
+ * 3. Buscar un Estudiante por Nombre: ○ Implementa una función que busque un estudiante por su nombre.
+ * Utiliza el método getDato(int pos)paraacceder a los datos de los nodos y busca al estudiante en la lista.
+ * 4. Eliminar un Estudiante por Nombre: ○ Implementa una función que elimine un estudiante por su nombre.
+ * Utiliza los métodos getDato(int pos)yremover(int pos)paraencontrar y eliminar al estudiante de la lista.
+ * 5. Contar Estudiantes: ○ UsaelmétodogetTamanio()paracontar el número de estudiantes en la lista y muestra el resultado.
+ */
+
+#include <iostream>
+#include "Lista/Lista.h"
+using namespace std;
